ddd520526f0d7c2e1fb802b321daf90751c5c6ed
ismail hassan <ismailshariff350@gmail.com>
2025-01-13 20:12:07 -0600

    hook in all elements by their class and ids plus all element

--- script.js
+++ script.js
@@ -2,19 +2,29 @@
 // Feel free to make this to what you want!
 // Example:
 // let fungusHP = 100;
+let ap= 100;
+let hp = 100;
 
 function onReady() {
     console.log("Ready to go!")
     
     // Make sure you check the index.html file! 
     // There are lots of buttons and things ready for you to hook into here!
+    const apText = document.querySelector('.ap-text'); // ap 100 class
+    const hpText = document.querySelector('.hp-text'); // hp 100 class
+    const apMeter = document.getElementById('ap-meter'); // id
+    const hpMeter = document.getElementById('hp-meter'); //id 
+    const fungus = document.querySelector('.freaky-fungus');
+    const attackButtons = document.querySelectorAll('.attack-btn'); //all attack buttons 
     
     
     // 🧠 Remember
     // - Handle events that ->
+
     // - Updates state which is ->
     // - Rendered to the DOM
 }
 
 
 onReady()
+
